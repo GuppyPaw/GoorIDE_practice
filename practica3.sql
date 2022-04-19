238ffda8edb3c25f6250ac59b76ff8b613f3a113
Andres <67043785+GuppyPaw@users.noreply.github.com>
2022-04-19 07:24:05 -0500

    Create practica3.sql

---
+++ practica3.sql
@@ -0,0 +1,6 @@
+-- Math
+
+1. We reviwed some math operators
+2. This included sum, subtraction, multiplication, factorial, etc.
+3. We applied this to data instead of numbers during queries
+4. Finally we checked the percentils
