8c5e1457983a02a70af0da23faad5bfc27c3d71c
Andres <67043785+GuppyPaw@users.noreply.github.com>
2022-04-26 08:16:39 -0500

    Created and finshed practica5

---
+++ practica5.sql
@@ -0,0 +1,11 @@
+-- Constraints
+
+1.- PRIMARY KEY is a column or group of columns that identify each row on a table
+
+2.- A FOREIGN KEY is a field in a table that makes a reference to the primary key in another table
+
+3.- CHECK can be used to validate if a field meets the specified criteria
+
+4.- UNIQUE is used to indicate that the data in the column must be unique in each row
+
+5.- NOT NULL is used so the column can't have null values
