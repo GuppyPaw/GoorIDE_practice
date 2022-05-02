b34d12355c4ab32f38c1ddea308997ca2f68cf23
Andres <67043785+GuppyPaw@users.noreply.github.com>
2022-05-02 09:01:55 -0500

    Created practica9

---
+++ practica9.sql
@@ -0,0 +1,9 @@
+-- Subquery
+
+1. Subqueries can be used in:
+  1.1 WHERE and WHERE IN
+  1.2 FROM
+  1.3 JOIN
+  1.4 As part of SELECT as calculated column
+
+2. Common table expressions, is a temporal table created from a subquery
